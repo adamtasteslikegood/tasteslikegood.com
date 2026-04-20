20bbf267901d85f6f4c34811bf6397ce97b79ae1
adam schoen <adam@tasteslikegood.com>
2026-04-20 12:45:23 -0700

    Fixed auth.py and blueprints to remove blocking over reafching gcloud scope

--- blueprints/auth_api_bp.py
+++ blueprints/auth_api_bp.py
@@ -30,7 +30,6 @@ SCOPES = [
     "openid",
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/cloud-platform",
 ]
 
 
