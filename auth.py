d85e3dd94b3d046b5b0b9716c2f5195cd0b9771a
adam schoen <adam@tasteslikegood.com>
2026-04-20 13:56:36 -0700

    fix: remove sensitive cloud-platform scope from user auth

--- auth.py
+++ auth.py
@@ -20,7 +20,6 @@ SCOPES = [
     "openid",
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/cloud-platform",
 ]
 
 
